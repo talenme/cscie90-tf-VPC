eb55e09c05afeb8534dc722bcb4903b1b45f96da
talenme <github@pyrian.com>
2022-10-25 21:15:16 -0400

    Add files via upload

---
+++ network.tf
@@ -0,0 +1,87 @@
+#Create VPC in us-east-1
+resource "aws_vpc" "vpc_master" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${terraform.workspace}-vpc"
+  }
+
+}
+
+#Get all available AZ's in VPC for master region
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "public_subnet" {
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "${terraform.workspace}-subnet"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc_master.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_master.id
+
+  tags = {
+    Name = "${terraform.workspace}-igw"
+  }
+}
+
+
+#Create SG for allowing TCP/22 from anywhere, THIS IS FOR TESTING ONLY
+resource "aws_security_group" "sg" {
+  name        = "${terraform.workspace}-sg"
+  description = "Allow TCP/22"
+  vpc_id      = aws_vpc.vpc_master.id
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow http traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${terraform.workspace}-securitygroup"
+  }
+}
