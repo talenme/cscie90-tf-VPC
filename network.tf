5601bbf7dab091bbe1d2953372895bd69fef1b78
talenme <github@pyrian.com>
2022-10-26 22:10:24 -0400

    Add files via upload
    removed the redundant route settings from subnet declaration

--- network.tf
+++ network.tf
@@ -26,11 +26,6 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc_master.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
   tags = {
     Name = "${terraform.workspace}-route-table"
   }
